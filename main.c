1a60b689776a7f531f0fdee410b75a2d8c20e396
amelie-thuerk <a.thuerk01@gmail.com>
2023-02-11 18:07:53 +0100

    used a loop to get, validate and check input

--- main.c
+++ main.c
@@ -4,67 +4,54 @@
 int main() {
     
     while (1) {
-        char dna1[100];
-        char dna2[100];
-        printf("first strand of DNA:\t");
-        scanf("%s", dna1);
         
-        if (strcmp(dna1, "q") == 0) {
-            printf("\nExiting...\n");
-            break;
+        char dna[2][100];
+        int is_not_valid = 0;
+        
+        for (int i = 0; i < 2; i++) {
+            printf("strand %d of DNA:\t", i + 1);
+            scanf("%s", dna[i]);
+            
+            if (strcmp(dna[i], "q") == 0) {
+                printf("\nExiting...\n");
+                return 0;
+            }
+            
+            for (int j = 0; j < strlen(dna[i]); j++) {
+                if (dna[i][j] != 'A' && dna[i][j] != 'C' && dna[i][j] != 'G' && dna[i][j] != 'T') {
+                    is_not_valid = 1;
+                    break;
+                }
+            }
         }
         
-        printf("second strand of DNA:\t");
-        scanf("%s", dna2);
+        int d1 = strlen(dna[0]);
+        int d2 = strlen(dna[1]);
         
-        if (strcmp(dna2, "q") == 0) {
-            printf("\nExiting...\n");
-            break;
-        }
-        
-        int d1 = strlen(dna1);
-        int d2 = strlen(dna2);
-        
-        if (d1 != d2) {
-            printf("\nBoth strands need to be the same lenght. Try again.\n\n");
+        if (is_not_valid) {
+            printf("Input is limited to A, C, G, T. Try again.\n\n");
             continue;
         }
         
-        int is_valid = 1;
-        
-        for (int i = 0; i < d1; i++) {
-            if (dna1[i] != 'A' && dna1[i] != 'C' && dna1[i] != 'G' && dna1[i] != 'T') {
-                is_valid = 0;
-                break;
-            }
-            
-            if (dna2[i] != 'A' && dna2[i] != 'C' && dna2[i] != 'G' && dna2[i] != 'T') {
-                is_valid = 0;
-                break;
-            }
-        }
-        
-        if (!is_valid) {
-            printf("Input is limited to A, C, G, T. Try again.\n\n");
+        if (d1 != d2) {
+            printf("\nBoth strands need to be the same length. Try again.\n\n");
             continue;
         }
         
         int hamming_distance = 0;
         
-        if (d1 == d2) {
-            printf("\t\t\t");
-            for (int i = 0; i < d1; i++) {
-                if (dna1[i] != dna2[i]) {
-                    printf("^");
-                    hamming_distance++;
-                } else {
-                    printf(" ");
-                }
+        printf("\t\t\t");
+        for (int i = 0; i < d1; i++) {
+            if (dna[0][i] != dna[1][i]) {
+                printf("^");
+                hamming_distance++;
+            } else {
+                printf(" ");
             }
-            
-            printf("\n\n");
-            printf("The Hamming Distance is %d.\n\n", hamming_distance);
-        } 
+        }
+        
+        printf("\n\n");
+        printf("The Hamming Distance is %d.\n\n", hamming_distance);
         
     }
     return 0;
